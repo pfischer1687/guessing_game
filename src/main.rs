e0da519196177246540652e1e85510c48154244d
pfischer1687 <pfisch789@outlook.com>
2023-06-14 00:02:08 -0700

    Tells the user if their guess was too high, too low, or correct

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,21 @@ fn main() {
     let guess: i32;
     
     println!("I have generated a random number between 1 and 100. Please keep entering your guesses until you get the correct answer.");
+
+    
     println!("Enter your guess:");
     _ = io::stdin().read_line(&mut buffer);
     guess = buffer.trim().parse().unwrap();
+
+    if guess == rand_num {
+        println!("Congratulations, your guess of {guess} was correct!")
+    }
+    else if guess > rand_num {
+        println!("Your guess of {guess} was too high.")
+    }
+    else {
+        println!("Your guess of {guess} was too low.")
+    }
 
     println!("rand_num: {rand_num}, guess: {guess}");
 }
