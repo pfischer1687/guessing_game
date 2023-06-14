dabfff819499965a9afb2d16c96cdf7ea5c5486c
pfischer1687 <pfisch789@outlook.com>
2023-06-13 23:56:35 -0700

    Prompts user for initial guess of the number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use rand::prelude::*;
+use std::io;
 
 fn main() {
-    let number: i32 = thread_rng().gen_range(1..101);
-    println!("{number}");
+    let rand_num: i32 = thread_rng().gen_range(1..101);
+    let mut buffer: String = String::new();
+    let guess: i32;
+    
+    println!("I have generated a random number between 1 and 100. Please keep entering your guesses until you get the correct answer.");
+    println!("Enter your guess:");
+    _ = io::stdin().read_line(&mut buffer);
+    guess = buffer.trim().parse().unwrap();
+
+    println!("rand_num: {rand_num}, guess: {guess}");
 }
