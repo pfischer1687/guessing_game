7a61228f8949f47c5016fe2745f9d000316abe03
pfischer1687 <pfisch789@outlook.com>
2023-06-22 23:01:31 -0700

    Sets guess to make valid input cases more readable

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut stdin_result: Result<usize, io::Error>;
     let mut _stdin: usize;
     let mut guess_result: Result<i32, ParseIntError>;
-    let mut _guess: i32;
+    let mut guess: i32;
     let num_iter: i32 = 5;
     let rand_num: i32 = thread_rng().gen_range(1..101);
     
@@ -29,33 +29,30 @@ fn main() {
 
         guess_result = buffer.trim().parse();
 
-        let _guess = match guess_result {
-            Ok(g) => {
-                if g > 100 || g < 1 {
-                    println!("Your guess of {g} was not between 1 and 100 inclusive.");
-                    continue;
-                }
-
-                if g > rand_num {
-                    println!("Your guess of {g} was too high.");
-                    continue;
-                }
-                else if g < rand_num {
-                    println!("Your guess of {g} was too low.");
-                    continue;
-                }
-                else {
-                    println!("Congratulations, you correctly guessed {rand_num} in {n} tries!");
-                    return;
-                }
-            }
+        guess = match guess_result {
+            Ok(g) => g,
             Err(e) => {
                 println!("Error: {e}");
                 continue;
             },
         };
-    }
 
+        if guess > 100 || guess < 1 {
+            println!("Your guess of {guess} was not between 1 and 100 inclusive.");
+            continue;
+        }
+
+        if guess > rand_num {
+            println!("Your guess of {guess} was too high.");
+        }
+        else if guess < rand_num {
+            println!("Your guess of {guess} was too low.");
+        }
+        else {
+            println!("Congratulations, you correctly guessed {rand_num} in {n} tries!");
+            return;
+        }
+    }
     println!("Max number of guesses ({num_iter}) exceeded.");
     return;
 }
